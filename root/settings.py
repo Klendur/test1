dfaa99707838686c765b051693e8751c9d788206
Klen <klenkertkorjus@gmail.com>
2025-04-16 21:33:29 +0300

    v16

--- root/settings.py
+++ root/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = 'django-insecure-2&gebr39@%qi74+thxia2dy6jx(pwl3m0%-_(cdms09bir04h)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['test1-0x72.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',

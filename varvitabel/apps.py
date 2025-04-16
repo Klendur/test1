0c526b7890827522142267f53092c413aec58fbf
Klen <klenkertkorjus@gmail.com>
2025-04-16 19:36:00 +0300

    v3

--- varvitabel/apps.py
+++ varvitabel/apps.py
@@ -4,3 +4,26 @@ from django.apps import AppConfig
 class VarvitabelConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'varvitabel'
+
+
+#create superuser
+from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
+
+class MyAppConfig(AppConfig):
+    name = 'myapp'
+
+    def ready(self):
+        self.create_superuser()
+
+    def create_superuser(self):
+        try:
+            # Check if superuser exists already
+            if not User.objects.filter(username='admin').exists():
+                User.objects.create_superuser(
+                    username='admin',
+                    email='admin@example.com',  # Replace with your email
+                    password='admin'  # Replace with your password
+                )
+        except ObjectDoesNotExist:
+            pass  # If any issues occur while creating, just pass

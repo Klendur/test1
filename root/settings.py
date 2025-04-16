c2cd90a13ab6562d0d391827d4dca9587a3dcbb6
Klen <klenkertkorjus@gmail.com>
2025-04-16 21:14:40 +0300

    v13

--- root/settings.py
+++ root/settings.py
@@ -38,7 +38,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['test1-0x72.onrender.com', 'localhost', '127.0.0.1']
 
-
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
 
 # Application definition
 

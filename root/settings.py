5b17bdd62fd355f2d71c3e7b2f80285b58c3e0df
Klen <klenkertkorjus@gmail.com>
2025-04-16 20:39:45 +0300

    v9

--- root/settings.py
+++ root/settings.py
@@ -9,11 +9,22 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Base directory path
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Static files will be collected from this directory
+STATICFILES_DIRS = [
+    BASE_DIR / 'varvitabel' / 'static',  # Make sure this directory exists
+]
+
+# This is where the collected static files will be stored
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # Use pathlib for consistency
+
+# URL to access static files
+STATIC_URL = '/static/'
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -114,7 +125,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
